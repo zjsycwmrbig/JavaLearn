2de614819ad8d60c7864a3f26324feb6e605a39f
zjsycwmrbig <2057544748@qq.com>
2023-02-21 08:35:30 +0800

    接口练习

--- GitHub.java
+++ GitHub.java
@@ -1,5 +1,21 @@
+interface gitable{
+    default void UseGit(){
+        System.out.println("Use Git");
+    }
+}
+class User implements  gitable{
+    String username;
+    User(){
+        this.username = "张靳生";
+    }
+    public void UseGit(){
+        System.out.println(this.username+"can "+"Use Git");
+    }
+}
 public class GitHub {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        User user = new User();
+        user.UseGit();
     }
 }
